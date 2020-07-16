61b50d28f40d5e25fd347567f92682f52b7888bc
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2020-07-16 23:00:30 +0530

    resolve windows path separator issue. v0.2.6 final

--- dist/FileContent.js
+++ dist/FileContent.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.FileContent = void 0;
 class FileContent {
     constructor(name, relativePath, content) {
         this.name = name;
@@ -8,4 +9,4 @@ class FileContent {
     }
 }
 exports.FileContent = FileContent;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiRmlsZUNvbnRlbnQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvRmlsZUNvbnRlbnQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxNQUFhLFdBQVc7SUFLcEIsWUFBWSxJQUFZLEVBQUUsWUFBbUIsRUFBRSxPQUFlO1FBQzFELElBQUksQ0FBQyxJQUFJLEdBQUcsSUFBSSxDQUFDO1FBQ2pCLElBQUksQ0FBQyxZQUFZLEdBQUcsWUFBWSxDQUFDO1FBQ2pDLElBQUksQ0FBQyxPQUFPLEdBQUcsT0FBTyxDQUFBO0lBQzFCLENBQUM7Q0FFSjtBQVhELGtDQVdDIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiRmlsZUNvbnRlbnQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvRmlsZUNvbnRlbnQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQUEsTUFBYSxXQUFXO0lBS3BCLFlBQVksSUFBWSxFQUFFLFlBQW1CLEVBQUUsT0FBZTtRQUMxRCxJQUFJLENBQUMsSUFBSSxHQUFHLElBQUksQ0FBQztRQUNqQixJQUFJLENBQUMsWUFBWSxHQUFHLFlBQVksQ0FBQztRQUNqQyxJQUFJLENBQUMsT0FBTyxHQUFHLE9BQU8sQ0FBQTtJQUMxQixDQUFDO0NBRUo7QUFYRCxrQ0FXQyJ9
